f7836f561df51095a3a836c20368f251e01d22e1
printfn <printfn@users.noreply.github.com>
2021-06-26 00:47:27 +1200

    Get the name of a specific base

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #[allow(dead_code)]
 mod base;
 
+use std::{env, error};
 use base::*;
 
-fn main() {
+fn loop_all_numbers() -> ! {
     let mut n = 1;
     let mut cache = Cache::default();
     loop {
@@ -11,5 +12,25 @@ fn main() {
         let abbr = find_abbreviation(n, &mut cache);
         println!("{}: {} ({})", n, name, abbr);
         n += 1;
+    }
+}
+
+fn parse_and_display_number(n: &str) -> Result<(), Box<dyn error::Error>> {
+    let n: i64 = n.parse()?;
+    let mut cache = Cache::default();
+    let name = BaseName(Base::new(n, &mut cache), true);
+    println!("{}", name);
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() <= 1 {
+        loop_all_numbers();
+    } else {
+        match parse_and_display_number(&args[1]) {
+            Ok(()) => (),
+            Err(e) => eprintln!("Error: {}", e),
+        }
     }
 }
